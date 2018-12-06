8888f07236e3c150febe7293e60d7c9d7d3af012
andrewlp8 <andyp@byui.edu>
2018-12-06 14:53:20 -0800

    update dynamic content

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -1,5 +1,38 @@
+// city variables
+var city = document.getElementById('city').innerText;
+
+var cityId = "";
+
+var preston = 5061036;
+
+var sodaSprings = 5678757;
+
+var fishHaven = 5052658;
+
+switch (city){
+    case "Preston": 
+    cityId = preston;
+    break;
+
+    case "Soda Springs":
+    cityId = sodaSprings;
+    break;
+
+    case "Fish Haven":
+    cityId = fishHaven;
+    break;
+}
+
+
+// weekly
+
+
+
+
+    
+// Daily
 var weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=48.83&lon=-115.83&units=imperial&async=true' + '&APPID=44854c54343626da5e75e87135a175f6');
+weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&units=imperial&async=true' + '&APPID=44854c54343626da5e75e87135a175f6');
 weatherRequest.onload = function () {
     
     var data = JSON.parse(weatherRequest.responseText);
@@ -28,7 +61,14 @@ weatherRequest.onload = function () {
         var chill = parseFloat(f).toFixed(2);
         document.getElementById('windChill').innerHTML = chill;
 
+        var wImg = document.getElementById('wImage');
 
+        var myImage = document.createElement('img');
+
+        myImage.src = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+        wImg.appendChild(myImage);
+
+        document.getElementById('imgDescription').innerHTML = "<p>" + data.weather[0].description + "</p>";
 
 };
 
