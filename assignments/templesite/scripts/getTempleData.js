ea0592e2ee357ab8c0bfba46f45b6f59252fad7f
andrewlp8 <andyp@byui.edu>
2018-12-16 13:48:27 -0800

    temple page update

--- assignments/templesite/scripts/getTempleData.js
+++ assignments/templesite/scripts/getTempleData.js
@@ -1,15 +1,101 @@
+
+
 var myRequest = new XMLHttpRequest();
-myRequest.open('GET', '');
+myRequest.open('GET', '\directory/temples.json');
+
+
+
 myRequest.onload = function () {
     var data = JSON.parse(myRequest.responseText);
 
-    $(function(){
+    copenhagenTitle(data);
+    showCopenhagenClosures(data);
+    SAfricaTitle(data);
+    showSAfricaClosures(data);
+    LVNevadaTitle(data);
+    showLVNevadaClosures(data);
+    portlandTitle(data);
+    showPortlandClosures(data);
 
-		var temple = data.temple;
+    // CopenHagen
+    function copenhagenTitle(jsonObj) {
+        var myh2 = document.createElement('h2');
+        myh2.textContent = jsonObj.temple[0].name;
+        document.getElementById('temple1Title').appendChild(myh2);
+    }
 
-	$.each( temple, function( index, element){
-        // $('.templeTextContainer').append('<li>Temple ' + element.name + ' </li>' + "<li>" + element.closures + "</li>");
-        console.log("Test data" + element.name);
-	})
-	})
+    function showCopenhagenClosures(jsonObj) {
+        var closureDate = jsonObj.temple[0].closures;
+
+        $.each( closureDate, function( index ) {
+            var dateData = closureDate[index].date;
+
+            var myP = document.createElement('p');
+            myP.textContent = dateData;
+            document.getElementById('copenhagenClosures').appendChild(myP);
+            console.log( index + ": " + dateData );
+          });
+    }
+
+    // South Africa
+    function SAfricaTitle(jsonObj) {
+        var myh2 = document.createElement('h2');
+        myh2.textContent = jsonObj.temple[1].name;
+        document.getElementById('temple2Title').appendChild(myh2);
+    }
+
+    function showSAfricaClosures(jsonObj) {
+        var closureDate = jsonObj.temple[1].closures;
+
+        $.each( closureDate, function( index ) {
+            var dateData = closureDate[index].date;
+
+            var myP = document.createElement('p');
+            myP.textContent = dateData;
+            document.getElementById('sAfricaClosures').appendChild(myP);
+            console.log( index + ": " + dateData );
+          });
+    }
+
+    // Las Vegas
+    function LVNevadaTitle(jsonObj) {
+        var myh2 = document.createElement('h2');
+        myh2.textContent = jsonObj.temple[2].name;
+        document.getElementById('temple3Title').appendChild(myh2);
+    }
+
+    function showLVNevadaClosures(jsonObj) {
+        var closureDate = jsonObj.temple[3].closures;
+
+        $.each( closureDate, function( index ) {
+            var dateData = closureDate[index].date;
+
+            var myP = document.createElement('p');
+            myP.textContent = dateData;
+            document.getElementById('lvNevadaClosures').appendChild(myP);
+            console.log( index + ": " + dateData );
+          });
+    }
+
+    // Portland
+    function portlandTitle(jsonObj) {
+        var myh2 = document.createElement('h2');
+        myh2.textContent = jsonObj.temple[3].name;
+        document.getElementById('temple4Title').appendChild(myh2);
+    }
+
+    function showPortlandClosures(jsonObj) {
+        var closureDate = jsonObj.temple[3].closures;
+
+        $.each( closureDate, function( index ) {
+            var dateData = closureDate[index].date;
+
+            var myP = document.createElement('p');
+            myP.textContent = dateData;
+            document.getElementById('portlandClosures').appendChild(myP);
+            console.log( index + ": " + dateData );
+          });
+    }
 }
+
+myRequest.send();
